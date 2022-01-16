6b868e34f489a822a3936037689534286c18d5e6
Jack Huang <its28604@gmail.com>
2022-01-16 18:49:10 +0800

    [Solved] 10. Regular Expression Matching.

--- Hard/10. Regular Expression Matching/Readme.md
+++ Hard/10. Regular Expression Matching/Readme.md
@@ -3,8 +3,8 @@
 ### Description
 
 Given an input string `s` and a pattern `p`, implement regular expression matching with support for `'.'` and `'*'` where:
-* `'.'` Matches any single character.
-* `'*'` Matches zero or more of the preceding element.
+ - `'.'` Matches any single character.
+ - `'*'` Matches zero or more of the preceding element.
 
 The matching should cover the **entire** input string (not partial).
 
@@ -34,8 +34,33 @@ Explanation: ".*" means "zero or more (*) of any character (.)".
 
 ### Constraints:
 
- * `1 <= s.length <= 20`
- * `1 <= p.length <= 30`
- * `s` contains only lowercase English letters.
- * `p` contains only lowercase English letters, `'.'`, and `'*'`.
- * It is guaranteed for each appearance of the character `'*'`, there will be a previous valid character to match.
+ - `1 <= s.length <= 20`
+ - `1 <= p.length <= 30`
+ - `s` contains only lowercase English letters.
+ - `p` contains only lowercase English letters, `'.'`, and `'*'`.
+ - It is guaranteed for each appearance of the character `'*'`, there will be a previous valid character to match.
+
+
+---
+
+### Thought:
+
+#### Situation
+
+ - Normal expression(without any special latters).
+ - Universal character expression `.*`
+ - Duplicate expression `a*`
+
+#### Approach
+ For time saving, doing the basic judgment first for the expression that is impossible matching.
+  - `s` = `"abc"`
+  - `p` = `".*a"`
+  - The `p` is ending by letter `'a'` and `s` is ending by letter `'c'`, so that is an impossible match.
+
+ Next, if universal character expression `.*` exists, skip it to figure out the necessary pattern in this match by recursion.
+
+ If it's not exists, comparing the character in `s` and `p` one by one (including `'.'`).
+
+ If it's a duplicate expression, try to findout how many duplicate is needed.
+
+ Finally, Recursion all situation, like the mouse through the maze.
--- Hard/10. Regular Expression Matching/solution.cpp
+++ Hard/10. Regular Expression Matching/solution.cpp
@@ -3,10 +3,14 @@ public:
     bool isMatch(string s, string p) {
         char* cur_s = s.data();
         char* cur_p = p.data();
+        bool comp = BasicJudgment(cur_s, cur_p);
+        if (!comp)
+            return false;
         return Compare(cur_s, cur_p);
     }
     
     bool Compare(char* cur_s, char* cur_p) {
+        cout << "Compare, cur_s:" << *cur_s << ", cur_p:" << *cur_p << '\n';
         if (*cur_p == *cur_s && *cur_p == '\0')
             return true;
 
@@ -26,25 +30,72 @@ public:
             return Compare(cur_s + 1, cur_p + 1);
         }
         else {
+            if (*cur_p == '\0')
+                return false;
+
             if (*next_p == '*') 
                 return Compare(cur_s, cur_p + 2);
-            else 
-                return false;
+
+            return false;
         }
     }
 
-    bool DuplicateExpression(char* cur_s, char* cur_p) {
-
-        char* next_p = cur_p + 1;
-        char* anext_p = cur_p + 2;
-
-        int atleast_count = 0;
-        while (*anext_p == *cur_p) {
-            atleast_count++;
-            anext_p++;
+    bool BasicJudgment(char* cur_s, char* cur_p) {
+        vector<string> nor_ex_list;
+        string nor_ex = "";
+        char* flag = cur_p;
+        while (*flag != '\0') {
+            char* next = flag + 1;
+            if (*next != '*') {
+                if (nor_ex == "") 
+                    nor_ex = *flag;
+                else 
+                    nor_ex += *flag;
+                flag += 1;
+            }
+            else {
+                if (nor_ex != "") {
+                    nor_ex_list.push_back(nor_ex);
+                    nor_ex = "";
+                }
+                flag += 2;
+            }
         }
 
+        if (nor_ex != ""){
+            nor_ex += '$';
+            nor_ex_list.push_back(nor_ex);
+        }
 
+        for (auto &v : nor_ex_list) {
+            cout << "BasicJudgment, v:" << v << '\n';
+            string s(cur_s);
+            if (!Contains(cur_s, v.data()))
+                return false;
+        }
+        return true;
+    }
+
+    bool Contains(char* cur_s, char* cur_p) {
+        if (*cur_p == '$')
+            return *cur_s == '\0';
+
+        if (*cur_p == '\0')
+            return true;
+
+        if (*cur_s == '\0')
+            return false;
+
+        if (*cur_s == *cur_p || *cur_p == '.') {
+            bool contains = Contains(cur_s + 1, cur_p + 1);
+            if (contains)
+                return true;
+        }
+
+        return Contains(cur_s + 1, cur_p);
+    }
+
+    bool DuplicateExpression(char* cur_s, char* cur_p) {
         char* flag_s = cur_s;
 
         int actual_count = 0;
@@ -52,16 +103,13 @@ public:
             actual_count++;
             flag_s++;
         }
-
-        if (actual_count >= atleast_count){
-            bool comp = false;
-            while (!comp && actual_count != 0) {
-                comp = Compare(cur_s + actual_count--, cur_p + 2 + atleast_count);
-            }
-            return comp;
-
+        bool comp = false;
+        while (!comp && actual_count >= 0) {
+            cout << "BasicJudgment, cur_s:" << *cur_s <<", actual_count:" << actual_count << '\n';
+            comp = Compare(cur_s + actual_count--, cur_p + 2);
+            cout << "_BasicJudgment, cur_s:" << *cur_s <<", actual_count:" << actual_count << '\n';
         }
-        else return false;
+        return comp;
     }
 
     bool UniversalCharExpression(char* cur_s, char* cur_p) {
